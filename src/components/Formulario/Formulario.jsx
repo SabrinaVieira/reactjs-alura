be95dbd4c8c315e76d50afcd6d798c7c61c30354
SabrinaVieira <sah.luciana@gmail.com>
2021-08-17 21:40:23 -0300

    :construction: using onchange event and this. to catch information

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -2,11 +2,16 @@ import React, { Component }  from 'react'
 import "./Formulario.css"
  
 export default class Formulario extends Component{
+
+    handleMudancaTitulo(evento){
+        console.log(evento.target.value)
+    }
+
     render(){
         return(
 
         <form className="form-cadastro">
-            <input  className="form-cadastro_input" type="text" placeholder="Título" />
+            <input className="form-cadastro_input" type="text" placeholder="Título" onChange={this.handleMudancaTitulo} />
             <textarea className="form-cadastro_input" rows={15} placeholder="Escreva sua nota..." />
             <button className="form-cadastro_input form-cadastro_submit">Criar nota</button>
         </form>
